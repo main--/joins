8e56dcc867b6ce8ef26c170937ac2135b388d043
main() <main@ehvag.de>
2019-05-19 16:39:22 +0200

    sortmerge

--- joins/src/main.rs
+++ joins/src/main.rs
@@ -59,7 +59,6 @@ impl<P, L, R> Stream for OrderedMergeJoin<P, L, R>
     type Error = L::Error;
 
     fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
-        let predicate = &self.predicate;
         let mut ret = None;
         while ret.is_none() {
             let poll_left = {
@@ -95,14 +94,80 @@ impl<P, L, R> Join<P, L, R> for OrderedMergeJoin<P, L, R>
           R: Stream<Error=L::Error>,
           L::Item: Clone + PartialOrd<R::Item>,
           R::Item: Clone {
-    fn build(predicate: P, mut left: L, right: R) -> Self {
+    fn build(predicate: P, left: L, right: R) -> Self {
         OrderedMergeJoin { predicate, left: left.peekable(), right: right.peekable() }
     }
 }
 
 
 
+enum SortMergeJoin<P, L: Stream, R: Stream> {
+    InputPhase {
+        predicate: P,
+        left: stream::Fuse<L>,
+        right: stream::Fuse<R>,
 
+        left_buf: Vec<L::Item>,
+        right_buf: Vec<R::Item>,
+    },
+    OutputPhase(OrderedMergeJoin<P, stream::IterOk<std::vec::IntoIter<L::Item>, L::Error>, stream::IterOk<std::vec::IntoIter<R::Item>, R::Error>>),
+    Tmp,
+}
+impl<P, L, R> Stream for SortMergeJoin<P, L, R>
+    where P: JoinPredicate<L::Item, R::Item>,
+          L: Stream,
+          R: Stream<Error=L::Error>,
+          L::Item: Clone + PartialOrd<R::Item> + Ord,
+          R::Item: Clone + Ord {
+    type Item = P::Output;
+    type Error = L::Error;
+
+    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+        loop {
+            match self {
+                SortMergeJoin::InputPhase { predicate, left, right, left_buf, right_buf } => {
+                    let l = left.poll()?;
+                    let r = right.poll()?;
+
+                    match (l, r) {
+                        (Async::Ready(None), Async::Ready(None)) => {
+                            // fall out of the match in order to replace self
+                        }
+                        (Async::NotReady, Async::NotReady) => return Ok(Async::NotReady),
+                        (l, r) => {
+                            if let Async::Ready(Some(l)) = l { left_buf.push(l); }
+                            if let Async::Ready(Some(r)) = r { right_buf.push(r); }
+
+                            continue;
+                        }
+                    }
+                }
+                SortMergeJoin::OutputPhase(omj) => return omj.poll(),
+                SortMergeJoin::Tmp => unreachable!(),
+            }
+
+            *self = match std::mem::replace(self, SortMergeJoin::Tmp) {
+                SortMergeJoin::InputPhase { predicate, left, right, mut left_buf, mut right_buf } => {
+                    left_buf.sort();
+                    right_buf.sort();
+                    SortMergeJoin::OutputPhase(OrderedMergeJoin::build(predicate, stream::iter_ok(left_buf), stream::iter_ok(right_buf)))
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+}
+
+impl<P, L, R> Join<P, L, R> for SortMergeJoin<P, L, R>
+    where P: JoinPredicate<L::Item, R::Item>,
+          L: Stream,
+          R: Stream<Error=L::Error>,
+          L::Item: Clone + PartialOrd<R::Item> + Ord,
+          R::Item: Clone + Ord {
+    fn build(predicate: P, mut left: L, right: R) -> Self {
+        SortMergeJoin::InputPhase { predicate, left: left.fuse(), right: right.fuse(), left_buf: Vec::new(), right_buf: Vec::new() }
+    }
+}
 
 /*
 TODO: impossible to implement, requires stream restart
@@ -180,9 +245,9 @@ impl<T: Clone> Stream for MemorySource<T> {
 
 fn main() {
     let left = stream::iter_ok::<_, ()>(vec![1,3,4,7,18]);
-    let right = stream::iter_ok(vec![0, 1, 3, 3,7,42,45]);
+    let right = stream::iter_ok(vec![88, 0, 1, 3, 3,7,42,45]);
 
-    let mut join = OrderedMergeJoin::build(EquiJoin::make(|x, y| x == y), left, right);
+    let mut join = SortMergeJoin::build(EquiJoin::make(|x, y| x == y), left, right);
 
 /*
     let left = MemorySource { vec: vec![1,3,18,4,7], index: 0 };
@@ -194,7 +259,10 @@ fn main() {
     let mut join = NestedLoopJoin::build(EquiJoin::make(|&x, &y| x == (y+0)), left, right);
 
     */
-    while let Async::Ready(Some(x)) = join.poll().unwrap() {
-        println!("{:?}", x);
+    loop {
+        match join.poll().unwrap() {
+            Async::Ready(None) => break,
+            x => println!("{:?}", x),
+        }
     }
 }
